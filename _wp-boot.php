6872e6451486fc51470bcacebc88fbe0b0ddb561
Saxon Stiller <saxon.stiller@gmail.com>
2014-07-25 03:27:54 -0500

    try a different way

--- _wp-boot.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ===================
-// Bootstrap WordPress
-// ===================
-if ( !defined( 'ABSPATH' ) )
-    define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
-require_once( ABSPATH . 'wp-settings.php' );
